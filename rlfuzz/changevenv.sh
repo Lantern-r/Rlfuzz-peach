6e053002965c8f551848cdf5e4eecc192cc9daba
realwatch <377233805@qq.com>
2022-03-31 03:19:35 -0700

    add regex for sample name

--- rlfuzz/changevenv.sh
+++ rlfuzz/changevenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/real/peach-master/venv/bin/activate venv
-python2 /home/real/peach-master/sample_analyse.py $1 ""$2"" $3
+python2 /home/real/peach-master/sample_analyse.py $1 $2 $3
 deactivate
